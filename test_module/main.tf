ec3b282db0c582eb6d4267fb4edd7137949ac595
Joseph Peter <josephvpeter7@gmail.com>
2023-01-22 16:03:32 -0500

    module security group added

--- test_module/main.tf
+++ test_module/main.tf
@@ -2,10 +2,12 @@
 
 module "my_ec2" {
   source = "./ec2-instance"
+  security_group = module.security_group_web.web_sg
 }
 
 
 
 module "security_group_web" {
   source = "./security_group"
+  
 }
--- test_module/security_group/variables.tf
+++ test_module/security_group/variables.tf
@@ -1,6 +1,6 @@
 # ---security_group/variables.tf ---
 # declare security_group
 
-variable "security_group" {
-  description = "security_group for the security group module"
-}
+# variable "security_group" {
+#   description = "security_group for the security group module"
+# }
